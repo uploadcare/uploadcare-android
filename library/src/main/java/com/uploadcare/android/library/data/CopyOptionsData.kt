56ef75afdaafd7836acddeab9c3e6220f17aa190
GIlyazitdinov Raphael <iraphaele@gmail.com>
2020-07-31 04:28:28 +0300

    Fix: Update serialized version of the "makePublic" variable according to API docs.

--- library/src/main/java/com/uploadcare/android/library/data/CopyOptionsData.kt
+++ library/src/main/java/com/uploadcare/android/library/data/CopyOptionsData.kt
@@ -1,7 +1,9 @@
 package com.uploadcare.android.library.data
 
+import com.squareup.moshi.Json
+
 internal data class CopyOptionsData(val source: String,
-                           val target: String? = null,
-                           val store: Boolean? = null,
-                           val makePublic: Boolean? = null,
-                           val pattern: String? = null)
+                                    val target: String? = null,
+                                    val store: Boolean? = null,
+                                    @Json(name = "make_public") val makePublic: Boolean? = null,
+                                    val pattern: String? = null)
